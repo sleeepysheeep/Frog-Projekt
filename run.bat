4d28d0ce87f6399372330f20d4c474b3729fcab0
sleeepysheeep <gisela.schaefer@gmx.at>
2024-11-25 18:08:22 +0100

    updated run.bat

--- run.bat
+++ run.bat
@@ -1,16 +1,43 @@
 @echo off
-REM Kompiliere das Haskell-Programm
-ghc -o Projekt Projekt.hs
+REM Stelle sicher, dass Cabal verfügbar ist
+echo Starte den Build-Prozess...
+
+REM Baue das Haskell-Projekt
+cabal build
+
+REM Überprüfen, ob der Build erfolgreich war
+if %errorlevel% neq 0 (
+    echo Fehler beim Build-Prozess!
+    pause
+    exit /b %errorlevel%
+)
 
 REM Verarbeite test.txt
 echo Verarbeite test.txt...
-.\Projekt "test.txt"
-echo Verarbeite test1.txt...
-.\Projekt "test1.txt"
-echo Verarbeite war_and_peace.txt...
-.\Projekt "war_and_peace.txt"
+cabal run Project -- "test.txt"
+if %errorlevel% neq 0 (
+    echo Fehler beim Verarbeiten von test.txt!
+    pause
+    exit /b %errorlevel%
+)
 
-REM Optional: Führe Tests aus, falls du welche definiert hast
-REM runTestTT TestCase
+REM Verarbeite test1.txt
+echo Verarbeite test1.txt...
+cabal run Project -- "test1.txt"
+if %errorlevel% neq 0 (
+    echo Fehler beim Verarbeiten von test1.txt!
+    pause
+    exit /b %errorlevel%
+)
+
+REM Verarbeite war_and_peace.txt
+echo Verarbeite war_and_peace.txt...
+cabal run Project -- "war_and_peace.txt"
+if %errorlevel% neq 0 (
+    echo Fehler beim Verarbeiten von war_and_peace.txt!
+    pause
+    exit /b %errorlevel%
+)
+
 echo Verarbeitung abgeschlossen!
 pause
